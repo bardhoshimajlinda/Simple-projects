464113fcce01e3b116b92ff5ebcec38cd23c45dd
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-02-03 12:44:02 +0100

    TO DO List

--- TO DO List/index.js
+++ TO DO List/index.js
@@ -27,3 +27,12 @@ listContainer.addEventListener("click", function(e) {
         saveData();
     }
 }, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
